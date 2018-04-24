1759b6b6b6ff98422f2684bcc2503a97e553a3ae
Kathy Kennedy <nomamanomama@yahoo.com>
2018-04-24 17:22:45 -0500

    updated friend.js and links

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,6 @@
 var friends = [{
     name: "Ahmed",
-    image: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    image: "http://alphaowners.com/wp-content/uploads/french-bulldog-679x483.jpg",
     scores: [1,1,1,1,1,2,3,4,5,1]
  } ];
 
